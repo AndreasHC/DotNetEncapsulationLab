b2e3f5b872461ecde9e29ae418162e4ab530f22d
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-03-08 14:31:13 +0100

    Third part of the exercise: input validations

--- DotNetEncapsulationLab/Person.cs
+++ DotNetEncapsulationLab/Person.cs
@@ -7,10 +7,48 @@
         private int age;
         private decimal salary;
 
-        public string FirstName { get { return firstName; } }
-        public string LastName { get { return lastName; } }
-        public int Age { get { return age; } }
-        public decimal Salary {  get { return salary; } }
+        public string FirstName
+        {
+            get { return firstName; }
+            private set
+            {
+                if (value.Length < 3)
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            private set
+            {
+                if (value.Length < 3)
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                lastName = value;
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            private set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get { return salary; }
+            private set
+            {
+                if (value < 460)
+                    throw new ArgumentException("Salary cannot be less than 460 dollar!");
+                salary = value;
+            }
+        }
+
+
 
         public override string ToString()
         {
@@ -18,17 +56,17 @@
         }
         public Person(string firstName, string lastName, int age, decimal salary)
         {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.age = age;
-            this.salary = salary;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
         }
 
         public void IncreaseSalary(decimal percentage)
         {
             if (age < 30)
                 percentage /= 2;
-            this.salary = Math.Round(this.salary*(1+percentage/100), 2);
+            Salary = Math.Round(this.salary * (1 + percentage / 100), 2);
         }
     }
 }
