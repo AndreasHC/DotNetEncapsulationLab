abc291b1ea09173f18adb99f9eb20eb2e7a756da
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-03-08 13:11:36 +0100

    Add project files.

---
+++ DotNetEncapsulationLab/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace DotNetEncapsulationLab
+{
+    internal class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public string FirstName { get { return firstName; } }
+        public string LastName { get { return lastName; } }
+        public int Age { get { return age; } }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
+        }
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+    }
+}
