1d2208a78a178bf8b9aa9a0dc4cabaa25404783f
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-03-08 14:02:19 +0100

    Second part of the exercise: salaries

--- DotNetEncapsulationLab/Person.cs
+++ DotNetEncapsulationLab/Person.cs
@@ -5,20 +5,30 @@
         private string firstName;
         private string lastName;
         private int age;
+        private decimal salary;
 
         public string FirstName { get { return firstName; } }
         public string LastName { get { return lastName; } }
         public int Age { get { return age; } }
+        public decimal Salary {  get { return salary; } }
 
         public override string ToString()
         {
-            return $"{FirstName} {LastName} is {Age} years old.";
+            return $"{FirstName} {LastName} receives {Salary:N2} dollars.";
         }
-        public Person(string firstName, string lastName, int age)
+        public Person(string firstName, string lastName, int age, decimal salary)
         {
             this.firstName = firstName;
             this.lastName = lastName;
             this.age = age;
+            this.salary = salary;
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (age < 30)
+                percentage /= 2;
+            this.salary = Math.Round(this.salary*(1+percentage/100), 2);
         }
     }
 }
